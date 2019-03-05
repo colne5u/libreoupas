aee7e8343c28f38eefeaf3ae0166f78f8045df46
Clément Colné <43238252+colne5u@users.noreply.github.com>
2019-03-05 08:22:30 +0100

    Delete db_connect.php

--- DB/db_connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-  /* connexion à la DB */
-  try {
-    $db = new PDO('mysql:host=localhost;dbname=m_terrassement;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-  }
-  catch(Exception $e) {
-    die('Erreur : ' . $e->getMessage());
-  }
-?>
