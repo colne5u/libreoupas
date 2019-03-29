cde469e81953b579dd31c1cefafcc6b13352e96b
Louis Abel <louis.abel5@etu.univ-lorraine.fr>
2019-03-29 08:25:00 +0100

    Fix

--- assets/js/index.js
+++ assets/js/index.js
@@ -72,7 +72,7 @@
     }
 
     function addOnlyFree() {
-        if ($("#onlyWindows").prop('checked')) {
+        if ($("#onlyFree").prop('checked')) {
             document.cookie = "ONLY_FREE=1; expires=Fri, 31 Dec 2100 23:59:59 GMT; path=/";
         } else {
             document.cookie = "ONLY_FREE=0; expires=Fri, 31 Dec 2100 23:59:59 GMT; path=/";
